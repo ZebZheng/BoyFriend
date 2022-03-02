c0476a39071e01ee078344447daa0fede94a07fd
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2022-03-02 13:54:49 +0800

    构造器移除frame

--- BoyFriend/Category/UIKit/UIButton/UIButton+BFInit.h
+++ BoyFriend/Category/UIKit/UIButton/UIButton+BFInit.h
@@ -5,40 +5,31 @@
 #import <UIKit/UIKit.h>
 
 @interface UIButton (BFInit)
-//MARK: -创建->位置-文本-颜色
-/// @param frame 位置
+/// 构建->文本-文本颜色
 /// @param title 文本
 /// @param titleColor 文本颜色
 + (UIButton *)bf_createWithFrame:(CGRect)frame
                         title:(NSString *)title
-                   titleColor:(UIColor *)titleColor;
-
-//MARK: -创建->位置-文本-颜色-字体
-/// @param frame 位置
+                      titleColor:(UIColor *)titleColor;
+/// 构建->文本-文本颜色-字体
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param font 文本字体
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
-                         font:(UIFont *)font;
-
-//MARK: -创建->位置-文本-颜色-字体-方法
-/// @param frame 位置
+                            font:(UIFont *)font;
+/// 构建->文本-文本颜色-字体
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param font 文本字体
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                          font:(UIFont *)font
                        target:(id)target
-                       action:(SEL)action;
-
-//MARK: -创建->位置-文本-颜色-字体-样式-背景-方法
-/// @param frame 位置
+                          action:(SEL)action;
+/// 构建->文本-文本颜色-字体-样式-背景颜色
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param font 文本字体
@@ -46,17 +37,14 @@
 /// @param bgColor 背景颜色
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                          font:(UIFont *)font
                    buttonType:(UIButtonType)buttonType
                       bgColor:(UIColor *)bgColor
                        target:(id)target
-                       action:(SEL)action;
-
-//MARK: - 构建->位置-文本-颜色-字体-样式-背景-圆角-方法
-/// @param frame 位置
+                          action:(SEL)action;
+/// 构建->文本-文本颜色-字体-样式-圆角-背景颜色
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param font 文本字体
@@ -65,55 +53,42 @@
 /// @param cornerRadius 圆角
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                      title:(NSString *)title
-                 titleColor:(UIColor *)titleColor
-                       font:(UIFont *)font
-                 buttonType:(UIButtonType)buttonType
-                    bgColor:(UIColor *)bgColor
-                     corner:(float)cornerRadius
-                     target:(id)target
-                     action:(SEL)action;
-
-
-//MARK: - 创建->位置-图片-选中图片
-/// @param frame 位置
++ (UIButton *)bf_createWithTitle:(NSString *)title
+                   titleColor:(UIColor *)titleColor
+                         font:(UIFont *)font
+                   buttonType:(UIButtonType)buttonType
+                      bgColor:(UIColor *)bgColor
+                       corner:(float)cornerRadius
+                       target:(id)target
+                          action:(SEL)action;
+/// 构建->图片-选中图片
 /// @param image 图片
 /// @param selectedImage 选中图片
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        image:(UIImage *)image
-                selectedImage:(UIImage *)selectedImage;
-
-//MARK: - 创建->位置-图片-选中图片-目标-函数
-/// @param frame 位置
++ (UIButton *)bf_createWithImage:(UIImage *)image
+                   selectedImage:(UIImage *)selectedImage;
+/// 构建->图片-选中图片-样式-圆角
 /// @param image 图片
 /// @param selectedImage 选中图片
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        image:(UIImage *)image
++ (UIButton *)bf_createWithImage:(UIImage *)image
                 selectedImage:(UIImage *)selectedImage
                        target:(id)target
-                       action:(SEL)action;
-
-//MARK: - 创建->位置-图片-选中图片-样式-圆角-目标-函数
-/// @param frame 位置
+                          action:(SEL)action;
+/// 构建->图片-选中图片-样式-圆角
 /// @param image 图片
 /// @param selectedImage 选中图片
 /// @param buttonType 样式
 /// @param cornerRadius 圆角
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        image:(UIImage *)image
++ (UIButton *)bf_createWithImage:(UIImage *)image
                 selectedImage:(UIImage *)selectedImage
                    buttonType:(UIButtonType)buttonType
                        corner:(float)cornerRadius
                        target:(id)target
-                       action:(SEL)action;
-
-//MARK: - 构建->位置-文本-文本颜色-选中文本-选中文本颜色-图片-选中图片-字体
-/// @param frame 位置
+                          action:(SEL)action;
+/// 构建->文本-文本颜色-选中文本-选中文本颜色-图片-选中图片-字体
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param selectedTitle 选中文本
@@ -121,17 +96,14 @@
 /// @param image 图片
 /// @param selectedImage 选中图片
 /// @param font 字体
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                 selectedTitle:(NSString *)selectedTitle
                 selectedColor:(UIColor *)selectedColor
                         image:(UIImage *)image
                 selectedImage:(UIImage *)selectedImage
-                         font:(UIFont *)font;
-
-//MARK: - 构建->位置-文本-文本颜色-选中文本-选中文本颜色-图片-选中图片-字体-样式-圆角
-/// @param frame 位置
+                            font:(UIFont *)font;
+/// 构建->文本-文本颜色-选中文本-选中文本颜色-图片-选中图片-字体-样式-圆角
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param selectedTitle 选中文本
@@ -143,8 +115,7 @@
 /// @param cornerRadius 圆角
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                 selectedTitle:(NSString *)selectedTitle
                 selectedColor:(UIColor *)selectedColor
@@ -154,7 +125,7 @@
                    buttonType:(UIButtonType)buttonType
                        corner:(float)cornerRadius
                        target:(id)target
-                       action:(SEL)action;
+                          action:(SEL)action;
 
 @end
 
--- BoyFriend/Category/UIKit/UIButton/UIButton+BFInit.m
+++ BoyFriend/Category/UIKit/UIButton/UIButton+BFInit.m
@@ -5,39 +5,37 @@
 #import "UIButton+BFInit.h"
 #import "UIFont+BFAdd.h"
 @implementation UIButton (BFInit)
-/// @param frame 位置
+/// 构建->文本-文本颜色
 /// @param title 文本
 /// @param titleColor 文本颜色
 + (UIButton *)bf_createWithFrame:(CGRect)frame
                         title:(NSString *)title
                    titleColor:(UIColor *)titleColor {
-    return [self bf_createWithFrame:frame title:title titleColor:titleColor font:BFPFRFontWithSizes(17) buttonType:UIButtonTypeCustom bgColor:nil corner:0 target:nil action:nil];
+    return [self bf_createWithTitle:title titleColor:titleColor font:nil buttonType:UIButtonTypeCustom bgColor:nil corner:0 target:nil action:nil];
 }
-/// @param frame 位置
+/// 构建->文本-文本颜色-字体
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param font 文本字体
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                          font:(UIFont *)font {
-    return [self bf_createWithFrame:frame title:title titleColor:titleColor font:font buttonType:UIButtonTypeCustom bgColor:nil corner:0 target:nil action:nil];
+    return [self bf_createWithTitle:title titleColor:titleColor font:font buttonType:UIButtonTypeCustom bgColor:nil corner:0 target:nil action:nil];
 }
-/// @param frame 位置
+/// 构建->文本-文本颜色-字体
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param font 文本字体
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                          font:(UIFont *)font
                        target:(id)target
                        action:(SEL)action {
-    return [self bf_createWithFrame:frame title:title titleColor:titleColor font:font buttonType:UIButtonTypeCustom bgColor:nil corner:0 target:target action:action];
+    return [self bf_createWithTitle:title titleColor:titleColor font:font buttonType:UIButtonTypeCustom bgColor:nil corner:0 target:target action:action];
 }
-/// @param frame 位置
+/// 构建->文本-文本颜色-字体-样式-背景颜色
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param font 文本字体
@@ -45,17 +43,16 @@
 /// @param bgColor 背景颜色
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                          font:(UIFont *)font
                    buttonType:(UIButtonType)buttonType
                       bgColor:(UIColor *)bgColor
                        target:(id)target
                        action:(SEL)action {
-    return [self bf_createWithFrame:frame title:title titleColor:titleColor font:font buttonType:buttonType bgColor:bgColor corner:0 target:target action:action];
+    return [self bf_createWithTitle:title titleColor:titleColor font:font buttonType:buttonType bgColor:bgColor corner:0 target:target action:action];
 }
-/// @param frame 位置
+/// 构建->文本-文本颜色-字体-样式-圆角-背景颜色
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param font 文本字体
@@ -64,8 +61,7 @@
 /// @param cornerRadius 圆角
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                          font:(UIFont *)font
                    buttonType:(UIButtonType)buttonType
@@ -73,79 +69,48 @@
                        corner:(float)cornerRadius
                        target:(id)target
                        action:(SEL)action {
-    UIButton *button = [UIButton buttonWithType:buttonType];
-    button.frame = frame;
-    if (title) {
-        [button setTitle:title forState:UIControlStateNormal];
-    }
-    if (titleColor) {
-        [button setTitleColor:titleColor forState:UIControlStateNormal];
-    }
-    if (font) {
-        button.titleLabel.font = font;
-    }
+    UIButton * button = [self bf_createWithTitle:title titleColor:titleColor selectedTitle:nil selectedColor:nil image:nil selectedImage:nil font:font buttonType:UIButtonTypeCustom corner:0 target:nil action:nil];
     if (bgColor) {
         button.backgroundColor = bgColor;
     }
-    if (cornerRadius > 0) {
-        button.layer.cornerRadius = cornerRadius;
-        button.layer.masksToBounds = YES;
-    }
-    [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
     return button;
 }
-/// @param frame 位置
+
+/// 构建->图片-选中图片
 /// @param image 图片
 /// @param selectedImage 选中图片
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        image:(UIImage *)image
++ (UIButton *)bf_createWithImage:(UIImage *)image
                 selectedImage:(UIImage *)selectedImage {
-    return [self bf_createWithFrame:frame image:image selectedImage:selectedImage buttonType:UIButtonTypeCustom corner:0 target:nil action:nil];
+    return [self bf_createWithImage:image selectedImage:selectedImage buttonType:UIButtonTypeCustom corner:0 target:nil action:nil];
 }
-/// @param frame 位置
+/// 构建->图片-选中图片-样式-圆角
 /// @param image 图片
 /// @param selectedImage 选中图片
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        image:(UIImage *)image
++ (UIButton *)bf_createWithImage:(UIImage *)image
                 selectedImage:(UIImage *)selectedImage
                        target:(id)target
                        action:(SEL)action {
-    return [self bf_createWithFrame:frame image:image selectedImage:selectedImage buttonType:UIButtonTypeCustom corner:0 target:target action:action];
+    return [self bf_createWithImage:image selectedImage:selectedImage buttonType:UIButtonTypeCustom corner:0 target:target action:action];
 }
-/// @param frame 位置
+/// 构建->图片-选中图片-样式-圆角
 /// @param image 图片
 /// @param selectedImage 选中图片
 /// @param buttonType 样式
 /// @param cornerRadius 圆角
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        image:(UIImage *)image
++ (UIButton *)bf_createWithImage:(UIImage *)image
                 selectedImage:(UIImage *)selectedImage
                    buttonType:(UIButtonType)buttonType
                        corner:(float)cornerRadius
                        target:(id)target
                        action:(SEL)action {
-    UIButton *button = [UIButton buttonWithType:buttonType];
-    button.frame = frame;
-    if (image) {
-        [button setImage:image forState:normal];
-    }
-    if (selectedImage) {
-        [button setImage:selectedImage forState:UIControlStateSelected];
-    }
-    if (cornerRadius > 0) {
-        button.layer.cornerRadius = cornerRadius;
-        button.layer.masksToBounds = YES;
-    }
-    [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
-    return button;
+    return [self bf_createWithTitle:nil titleColor:nil selectedTitle:nil selectedColor:nil image:image selectedImage:selectedImage font:nil buttonType:buttonType corner:cornerRadius target:target action:action];
 }
 
-/// 构建->位置-文本-文本颜色-选中文本-选中文本颜色-图片-选中图片-字体
-/// @param frame 位置
+/// 构建->文本-文本颜色-选中文本-选中文本颜色-图片-选中图片-字体
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param selectedTitle 选中文本
@@ -153,18 +118,16 @@
 /// @param image 图片
 /// @param selectedImage 选中图片
 /// @param font 字体
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                 selectedTitle:(NSString *)selectedTitle
                 selectedColor:(UIColor *)selectedColor
                         image:(UIImage *)image
                 selectedImage:(UIImage *)selectedImage
                          font:(UIFont *)font {
-    return [self bf_createWithFrame:frame title:title titleColor:titleColor selectedTitle:selectedTitle selectedColor:selectedColor image:image selectedImage:selectedImage font:font buttonType:UIButtonTypeCustom corner:0 target:nil action:nil];
+    return [self bf_createWithTitle:title titleColor:titleColor selectedTitle:selectedTitle selectedColor:selectedColor image:image selectedImage:selectedImage font:font buttonType:UIButtonTypeCustom corner:0 target:nil action:nil];
 }
-/// 构建->位置-文本-文本颜色-选中文本-选中文本颜色-图片-选中图片-字体-样式-圆角
-/// @param frame 位置
+/// 构建->文本-文本颜色-选中文本-选中文本颜色-图片-选中图片-字体-样式-圆角
 /// @param title 文本
 /// @param titleColor 文本颜色
 /// @param selectedTitle 选中文本
@@ -176,8 +139,7 @@
 /// @param cornerRadius 圆角
 /// @param target 目标
 /// @param action 函数
-+ (UIButton *)bf_createWithFrame:(CGRect)frame
-                        title:(NSString *)title
++ (UIButton *)bf_createWithTitle:(NSString *)title
                    titleColor:(UIColor *)titleColor
                 selectedTitle:(NSString *)selectedTitle
                 selectedColor:(UIColor *)selectedColor
@@ -189,7 +151,6 @@
                        target:(id)target
                        action:(SEL)action {
     UIButton *button = [UIButton buttonWithType:buttonType];
-    button.frame = frame;
     if (title) {
         [button setTitle:title forState:UIControlStateNormal];
     }
