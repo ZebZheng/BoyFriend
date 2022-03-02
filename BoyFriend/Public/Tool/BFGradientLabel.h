a6544da5724dcd66c91a97748f6e2641846fb921
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2022-03-02 17:32:10 +0800

    渐变label

---
+++ BoyFriend/Public/Tool/BFGradientLabel.h
@@ -0,0 +1,12 @@
+/*
+  Created by BF on 2021
+  Copyright © 2021年 BF. All rights reserved.
+*/
+
+#import <UIKit/UIKit.h>
+
+@interface BFGradientLabel : UILabel
+
+@property(nonatomic, strong) NSArray* colors;
+
+@end
