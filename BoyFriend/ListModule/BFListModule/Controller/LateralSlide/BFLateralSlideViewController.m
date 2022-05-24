1afc8efe82f1bf9317c2303c1d166b2506eeee71
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2022-05-24 16:53:33 +0800

    播放视频

--- BoyFriend/ListModule/BFListModule/Controller/LateralSlide/BFLateralSlideViewController.m
+++ BoyFriend/ListModule/BFListModule/Controller/LateralSlide/BFLateralSlideViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "BFLateralSlideViewController.h"
+#import "WMPlayer.h"
 
 @interface BFLateralSlideViewController ()
+
+@property(nonatomic, strong) WMPlayer * wmPlayer;
 
 @end
 
@@ -20,6 +23,27 @@
     
     [self bf_navTitle:@"抽屉"];
     
+    WMPlayerModel *playerModel = [WMPlayerModel new];
+    playerModel.title = @"测试网络播放";
+    playerModel.videoURL = [NSURL URLWithString:@"https://media.w3.org/2010/05/sintel/trailer.mp4"];
+    self.wmPlayer = [[WMPlayer alloc]initPlayerModel:playerModel];
+    [self.view addSubview:self.wmPlayer];
+    [self.wmPlayer mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.view);
+        make.top.mas_equalTo(100);
+        make.width.mas_equalTo(BFRatio(200));
+        make.height.mas_equalTo(self.wmPlayer.mas_width).multipliedBy(9.0/16);
+    }];
+    [self.wmPlayer play];
+}
+///视频切换
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.wmPlayer resetWMPlayer];
+    WMPlayerModel *playerModel = [WMPlayerModel new];
+    playerModel.title = @"网络播放";
+    playerModel.videoURL = [NSURL URLWithString:@"https://www.w3school.com.cn/example/html5/mov_bbb.mp4"];
+    self.wmPlayer.playerModel = playerModel;
+    [self.wmPlayer play];
 }
 
 /*
